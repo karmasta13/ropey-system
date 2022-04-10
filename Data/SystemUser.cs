0d65360236c3294fac4617e3d690f9d20d2e428d
karmasta13 <labbikarmacharya@gmail.com>
2022-04-10 22:24:30 +0545

    tk-600: Implemented login system from Asp.net Core Identity

---
+++ Data/SystemUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RopeyDVDSystem.Data
+{
+    public class SystemUser : IdentityUser 
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? UserType { get; set; }
+    }
+}
