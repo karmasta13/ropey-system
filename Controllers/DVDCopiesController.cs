8b18cfdefc9025962d08373d90778333c564a8be
Grishhhhha <grishagiri2002@gmail.com>
2022-05-08 15:36:20 +0545

    tk-614: feature 10

--- Controllers/DVDCopiesController.cs
+++ Controllers/DVDCopiesController.cs
@@ -18,5 +18,92 @@ namespace RopeyDVDSystem.Controllers
             var allDVDCopies = await _context.DVDCopies.ToListAsync();
             return View();
         }
+
+        public async Task<IActionResult> OlderThan365Days()
+        {
+
+            //Get Distinct Copy Numbers of Loaned DVDs
+            var loanedCopy = (from loan in _context.Loans
+                                 where loan.DateReturn == null
+                                 select loan.CopyNumber).Distinct();
+
+            //Get Data of Copies that have not been loaned
+            var notLoanedCopy = (from copy in _context.DVDCopies
+                                    join dvdtitle in _context.DVDTitles on copy.DVDNumber equals dvdtitle.DVDNumber
+                                    where !(loanedCopy).Contains(copy.CopyNumber)
+                                    select new
+                                    {
+                                        CopyNumber = copy.CopyNumber,
+                                        DVDTitle = dvdtitle.DVDTitleName,
+                                        DatePurchased = copy.DatePurchased
+                                    });
+
+            return View(await notLoanedCopy.ToListAsync());
+        }
+
+        // GET: DVDCopy/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dVDCopyModel = await _context.DVDCopies
+                .Include(d => d.DVDTitle)
+                .FirstOrDefaultAsync(m => m.CopyNumber == id);
+            if (dVDCopyModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(dVDCopyModel);
+        }
+
+        // POST: DVDCopy/Delete/5
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var dVDCopyModel = await _context.DVDCopies.FindAsync(id);
+            _context.DVDCopies.Remove(dVDCopyModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(OlderThan365Days));
+        }
+
+        public async Task<IActionResult> RemoveAll()
+        {
+            //Get Distinct Copy Numbers of Loaned DVDs
+            var loanedCopy = (from loan in _context.Loans
+                                 where loan.DateReturn == null
+                                 select loan.CopyNumber).Distinct();
+
+            //Get Data of Copies that have not been loaned
+            var notLoanedCopy = (from copy in _context.DVDCopies
+                                    join dvdtitle in _context.DVDTitles on copy.DVDNumber equals dvdtitle.DVDNumber
+                                    where !(loanedCopy).Contains(copy.CopyNumber)
+                                    select new
+                                    {
+                                        CopyNumber = copy.CopyNumber,
+                                        DVDTitle = dvdtitle.DVDTitleName,
+                                        DatePurchased = copy.DatePurchased
+                                    });
+
+            foreach (var copy in notLoanedCopy.ToList())
+            {
+                //Check if the Copy is older than one year.
+                if (DateTime.Now.Subtract(copy.DatePurchased).Days > 365)
+                {
+                    //Remove the Copy from the Database if the Copy is older than one year.
+                    var remove = (from removeCopy in _context.DVDCopies
+                                  where removeCopy.CopyNumber == copy.CopyNumber
+                                  select removeCopy).FirstOrDefault();
+                    _context.DVDCopies.Remove(remove);
+
+                }
+            }
+            //Save the changes made to the database.
+            _context.SaveChanges();
+            return RedirectToAction("OlderThan365Days");
+        }
     }
 }
