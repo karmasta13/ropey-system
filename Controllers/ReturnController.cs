7af3c858f08d4663daf6b84bfe7660ef30dcd190
karmasta13 <labbikarmacharya@gmail.com>
2022-05-08 15:50:01 +0545

    tk-615: Feature 5, 7, 11

--- Controllers/ReturnController.cs
+++ Controllers/ReturnController.cs
@@ -49,6 +49,130 @@ namespace RopeyDVDSystem.Controllers
 
             return View(loanRecord);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(string request)
+        {
+            string CopyNumber = Request.Form["SearchCopyNumber"];
+            ViewBag.SearchCopyNumber = CopyNumber;
+
+            // Get a list of all DVD Copy that are on loan
+            ViewBag.LoanedCopyNumberList = (string)System.Text.Json.JsonSerializer.Serialize(_context.DVDCopies.Select(x => x.CopyNumber).ToList());
+
+            if (CopyNumber != null &&
+                int.TryParse(CopyNumber, out int copyNumber) &&
+                _context.DVDCopies.Where(x => x.CopyNumber == copyNumber).Count() > 0)
+            {
+                var loanRecord = (from dt in _context.DVDTitles
+                                  join dtc in _context.DVDCategories on dt.CategoryNumber equals dtc.CategoryNumber
+                                  join dc in _context.DVDCopies on dt.DVDNumber equals dc.DVDNumber
+                                  join l in _context.Loans on dc.CopyNumber equals l.CopyNumber
+                                  join m in _context.Members on l.MemberNumber equals m.MemberNumber
+                                  orderby l.DateOut descending
+                                  where dc.CopyNumber == copyNumber
+                                  select new ReturnModel
+                                  {
+                                      CopyNumber = dc.CopyNumber,
+                                      DVDTitleName = dt.DVDTitleName,
+                                      DVDCategory = dtc.CategoryName,
+                                      DateOut = l.DateOut,
+                                      DateDue = l.DateDue,
+                                      DateReturn = l.DateReturn,
+                                      MemberName = m.MemberFirstName + ' ' + m.MemberLastName,
+                                      LoanNumber = l.LoanNumber,
+                                      Payment = l.ReturnAmount
+                                  });
+
+                if (loanRecord.Count() > 0)
+                {
+                    var loanRecordFirst = loanRecord.First();
+                    if (loanRecordFirst.DateReturn != DateTime.MinValue) loanRecordFirst.OverDue = 1;
+                    ViewData["LoanRecord"] = loanRecordFirst;
+                }
+                return View();
+            }
+            else if (CopyNumber == "")
+            {
+                // Get all the loan records
+                IEnumerable<ReturnModel> loanRecord = GetAllLoanRecords();
+                return View(loanRecord);
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
+
+
+        public IActionResult Confirmation(int LoanID)
+        {
+            if (LoanID == 0 ||
+                _context.Loans.Where(l => l.LoanNumber == LoanID).Count() == 0 ||
+                _context.Loans.Where(l => l.LoanNumber == LoanID).First().DateReturn != DateTime.MinValue)
+            {
+                return RedirectToAction("Index");
+            }
+
+            // Get the loan record
+            ReturnModel currentLoan = (from dt in _context.DVDTitles
+                                          join dtc in _context.DVDCategories on dt.CategoryNumber equals dtc.CategoryNumber
+                                          join dc in _context.DVDCopies on dt.DVDNumber equals dc.DVDNumber
+                                          join l in _context.Loans on dc.CopyNumber equals l.CopyNumber
+                                          join m in _context.Members on l.MemberNumber equals m.MemberNumber
+                                          where l.LoanNumber == LoanID
+                                          select new ReturnModel
+                                          {
+                                              CopyNumber = dc.CopyNumber,
+                                              DVDTitleName = dt.DVDTitleName,
+                                              DVDCategory = dtc.CategoryName,
+                                              DateOut = l.DateOut,
+                                              DateDue = l.DateDue,
+                                              MemberName = m.MemberFirstName + ' ' + m.MemberLastName,
+                                              LoanNumber = l.LoanNumber,
+                                              StandardCharge = dt.StandardCharge,
+                                              PenaltyCharge = dt.PenaltyCharge
+                                          }).First();
+
+            var today = DateTime.Today;
+            var overDueDays = (today - currentLoan.DateDue).TotalDays;
+            if (overDueDays < 0) overDueDays = 0;
+
+            currentLoan.OverDue = (int)overDueDays;
+            currentLoan.Payment = currentLoan.StandardCharge + currentLoan.PenaltyCharge * (decimal)overDueDays;
+            currentLoan.PenaltyCharge = currentLoan.PenaltyCharge * (decimal)overDueDays;
+
+            ViewData["Return"] = currentLoan;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Confirmation(ReturnModel returnDVD)
+        {
+            if (returnDVD.LoanNumber == 0 ||
+                returnDVD.CopyNumber == 0 ||
+                _context.Loans.Where(l => l.LoanNumber == returnDVD.LoanNumber).Count() == 0 ||
+                _context.DVDCopies.Where(dc => dc.CopyNumber == returnDVD.CopyNumber).Count() == 0 ||
+                _context.DVDCopies.Where(dc => dc.CopyNumber == returnDVD.CopyNumber).First().IsLoan == false)
+            {
+                return RedirectToAction("Index");
+            }
+
+            // Update the loan record
+            var loan = _context.Loans.Find(returnDVD.LoanNumber);
+            loan.DateReturn = DateTime.Today;
+            loan.ReturnAmount = returnDVD.Payment;
+
+            // Update the DVD copy record
+            var copy = _context.DVDCopies.Find(returnDVD.CopyNumber);
+            copy.IsLoan = false;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 
 }
